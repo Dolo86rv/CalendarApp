a46210dacf5be2967089ab63aa435976ade91b4a
Dolorv86 <dolorv86@gmail.com>
2023-03-27 13:20:14 +0200

    se realizan pruebas en el Store

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -3,9 +3,10 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete } from "../"
 import { localizer, getMessagesES } from '../../helpers';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useUiStore } from '../../hooks/useUiStore';
 import { useCalendarStore } from '../../hooks/useCalendarStore';
+import { useAuthStore } from '../../hooks';
 
 /*const events = [
     {
@@ -22,13 +23,18 @@ import { useCalendarStore } from '../../hooks/useCalendarStore';
 
 export const CalendarPage = () => {
 
+    const { user } = useAuthStore()
     const { openDateModal } = useUiStore()
-    const { events, setActiveEvent } = useCalendarStore()
+    const { events, setActiveEvent, startLoadingEvents } = useCalendarStore()
+
     const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week')
-    const eventStyleGetter = ( event, start, end, isSelected ) => {
     
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        
+        const isMyEvent = (user.uid === event.user._id)|| (user.uid === event.user.uid)
+
         const style = {
-            backgroundColor:'#347CF7',
+            backgroundColor: isMyEvent? '#347CF7' : '#465660',
             borderRadius: '0px',
             opacity: 0.8,
             color: 'white'
@@ -38,7 +44,7 @@ export const CalendarPage = () => {
         }
     }
 
-    const onDoubleClick = (event) => {
+    const onDoubleClick = () => {
         openDateModal()
     }
 
@@ -51,8 +57,11 @@ export const CalendarPage = () => {
         localStorage.setItem('lastView', event)
         setLastView(event)
     }
-    
 
+    useEffect(() => {
+        startLoadingEvents()
+    }, [])
+    
     return (
         <>
             <Navbar />
